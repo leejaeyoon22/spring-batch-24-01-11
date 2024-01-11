910435e4eb59465fe41566d52335c2cda934468b
leejaeyoon22 <tksska1514@gmail.com>
2024-01-11 14:34:41 +0900

    sbb mission1 Hello World 출력

---
+++ src/main/java/com/ll/sbb240111/Sbb240111Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbb240111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb240111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb240111Application.class, args);
+    }
+
+}
